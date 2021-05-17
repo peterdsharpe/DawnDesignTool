2e5640806ab086a77c2a49a873204b1363a58ddc
Peter Sharpe <peterdsharpe@gmail.com>
2021-05-17 12:07:03 -0400

    RBF works great! :D

--- test_raw_xfoil_data.py
+++ test_raw_xfoil_data.py
@@ -1,26 +1,46 @@
 from repickle_airfoil import *
 
+# fig, ax = plt.subplots()
+# plt.tricontourf(
+#     np.array(reynolds_list),
+#     np.array(alpha_list),
+#     np.array(cl_values)
+# )
+# cbar = fig.colorbar(cs)
+# ax.set_xscale("log")
+# ax.set(xlabel="Reynolds Number", ylabel=r"$\alpha$ (angle)",
+#        title=r"$C_l$ Raw from XFoil")
+# plt.show()
+#
+# fig, ax = plt.subplots()
+# plt.contourf(
+#     reynolds,
+#     alpha,
+#     grid_cl.T
+# )
+# cbar = fig.colorbar(cs)
+# ax.set_xscale("log")
+# ax.set(xlabel="Reynolds Number", ylabel=r"$\alpha$ (angle)",
+#        title=r"$C_l$ after griddata()")
+# plt.show()
+
+
+
 fig, ax = plt.subplots()
-plt.tricontourf(
-    np.array(reynolds_list),
+cl_rbf = Rbf(
+    np.log(np.array(reynolds_list)),
     np.array(alpha_list),
-    np.array(cl_values)
+    np.array(cl_values),
+    function='linear',
+    smooth=1,
 )
-cbar = fig.colorbar(cs)
-ax.set_xscale("log")
-ax.set(xlabel="Reynolds Number", ylabel=r"$\alpha$ (angle)",
-       title=r"$C_l$ Raw from XFoil")
-plt.show()
-
-
-fig, ax = plt.subplots()
+Reynolds, Alpha = np.meshgrid(reynolds, alpha, indexing="ij")
 plt.contourf(
     reynolds,
     alpha,
-    grid_cl.T
+    cl_rbf(np.log(Reynolds.flatten()), Alpha.flatten()).reshape(len(reynolds), len(alpha)).T
 )
-cbar = fig.colorbar(cs)
 ax.set_xscale("log")
 ax.set(xlabel="Reynolds Number", ylabel=r"$\alpha$ (angle)",
-       title=r"$C_l$ after griddata()")
+       title=r"$C_l$ after RBF")
 plt.show()
