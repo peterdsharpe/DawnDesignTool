b30b2958d7ff435edc1395062d94dcbb261acf00
ajdewald <62044249+ajdewald@users.noreply.github.com>
2022-04-23 21:00:32 -0400

    implemented conversion from db to power ratio for snr

--- design_opt.py
+++ design_opt.py
@@ -941,7 +941,7 @@ power_out_avionics = 180  # Pulled from Avionics spreadsheet on 5/13/20
 c = 299792458  # [m/s] speed of light
 k_b = 1.38064852E-23  # [m2 kg s-2 K-1]
 required_resolution = opti.parameter(value=2)  # meters from conversation with Brent on 2/18/22
-required_snr = opti.parameter(value=0)  # dB from conversation w Brent on 2/18/22
+required_snr = opti.parameter(value=20)  # dB from conversation w Brent on 2/18/22
 antenna_gain = opti.parameter(value=0.8)  # TODO check this
 center_wavelength = opti.parameter(value=0.226)  # meters
 sigma0 = opti.parameter(value=1)  # meters ** 2 ranges from
@@ -997,7 +997,7 @@ max_length_synth_ap = center_wavelength * dist / radar_length
 ground_area = swath_range * swath_azimuth * np.pi / 4
 radius = (swath_azimuth + swath_range) / 4
 ground_imaging_offset = np.sin(look_angle) * dist
-scattering_cross_sec = sigma0
+scattering_cross_sec = sigma0 # Todo change from db to ratio
 # scattering_cross_sec = np.pi * radius ** 2radar_offset_length =
 antenna_gain = 4 * np.pi * radar_area * 0.7 / center_wavelength ** 2
 pulse_duration = 1 / bandwidth
@@ -1015,8 +1015,10 @@ noise_power_density = k_b * T * bandwidth / (center_wavelength ** 2)
 power_trans = peak_power * pulse_duration
 power_received = power_trans * antenna_gain * radar_area * scattering_cross_sec / ((4 * np.pi) ** 2 * dist ** 4)
 power_out_payload = power_trans * pulse_rep_freq
+snr = power_received / noise_power_density
+snr_db = 10 * np.log10(snr)
 opti.subject_to([
-    required_snr <= power_received / noise_power_density,
+    # required_snr <= snr_db,
     pulse_rep_freq >= 2 * groundspeed / radar_length,
     pulse_rep_freq <= c / (2 * swath_azimuth),
     radar_width <= 0.4,
