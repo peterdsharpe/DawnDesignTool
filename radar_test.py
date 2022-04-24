cc88c9669e1ac827c182041c6bd393bb4ca2c9d4
ajdewald <62044249+ajdewald@users.noreply.github.com>
2022-04-24 10:23:17 -0400

    Have test case running in radar_test at only a single point in time to debug

--- radar_test.py
+++ radar_test.py
@@ -4,18 +4,16 @@ import aerosandbox as asb
 
 ### Payload Module
 opti = asb.Opti()
-minimize = "power_out_payload.mean()"
 
 c = 299792458 # [m/s] speed of light
 k_b = 1.38064852E-23 # [m2 kg s-2 K-1]
 required_resolution = opti.parameter(value=2) # 1-2 meters required from conversation with Brent on 2/18/22
 required_snr = opti.parameter(value=20)  # 6 dB min and 20 dB ideally from conversation w Brent on 2/18/22
 center_wavelength = opti.parameter(value=0.226) # meters from GAMMA Remote Sensing Doc
-n_timesteps = 120
-groundspeed = opti.parameter(value=30) * np.ones(n_timesteps) # average groudspeed
+groundspeed = opti.parameter(value=30) # average groudspeed
 T = opti.parameter(value=216)
 y = opti.parameter(value=12000)
-sigma0 = opti.parameter(value=1)
+sigma0_db = opti.parameter(value=-10)
 
 radar_length = opti.variable(
     init_guess=0.1,
@@ -30,28 +28,24 @@ radar_width = opti.variable(
     lower_bound=0,
 )
 bandwidth = opti.variable(
-    n_vars=n_timesteps,
     init_guess=1e6,
     scale=1e4,
     lower_bound=0,
     category='ops'
 )  # Hz
 peak_power = opti.variable(
-    n_vars=n_timesteps,
     init_guess=1000,
     scale=100,
     lower_bound=0,
     category='ops'
 )  # Watts
 pulse_rep_freq = opti.variable(
-    n_vars=n_timesteps,
     init_guess=10000,
     scale=1000,
     lower_bound=0,
     category='ops'
 )
 power_out_payload = opti.variable(
-    n_vars=n_timesteps,
     init_guess=1,
     scale=0.1,
     lower_bound=0,
@@ -68,7 +62,8 @@ max_length_synth_ap = center_wavelength * dist / radar_length
 ground_area = swath_range * swath_azimuth * np.pi / 4
 radius = (swath_azimuth + swath_range) / 4
 ground_imaging_offset = np.sin(look_angle) * dist
-scattering_cross_sec = sigma0
+sigma0 = 10 ** (sigma0_db / 10)
+scattering_cross_sec = sigma0 # Todo change from db to ratio
 # scattering_cross_sec = np.pi * radius ** 2radar_offset_length =
 antenna_gain = 4 * np.pi * radar_area * 0.7 / center_wavelength ** 2
 pulse_duration = 1 / bandwidth
@@ -96,6 +91,7 @@ opti.subject_to([
     radar_length <= 0.4,
 ])
 
+minimize = "power_out_payload"
 objective = eval(minimize)
 opti.minimize(objective)
 sol = opti.solve(max_iter=1000)
