db281207d4b9b145608c947011e7961b4446b50e
ajdewald <62044249+ajdewald@users.noreply.github.com>
2022-04-27 18:04:34 -0400

    set up sweep for required_headway_per_day

--- sensitivity_sweep.py
+++ sensitivity_sweep.py
@@ -5,16 +5,16 @@ import aerosandbox.numpy as np
 import pandas as pd
 
 ### Set the run ID
-run_name = "min_cruise_altitude"
+run_name = "required_headway_per_day"
 
 
-def run(alt_val):
+def run(dist):
     print("\n".join([
         "-" * 50,
-        f"Minimum Cruise Altitude: {alt_val}",
+        f"required_headway_per_day: {dist}",
     ]))
 
-    opti.set_value(min_cruise_altitude, alt_val)
+    opti.set_value(required_headway_per_day, dist)
 
     try:
         sol = func_timeout(
@@ -22,7 +22,7 @@ def run(alt_val):
             func=opti.solve,
             args=(),
             kwargs={
-                "max_iter": 200,
+                "max_iter": 2000,
                 "options" : {
                     "ipopt.max_cpu_time": 60,
                 },
@@ -41,7 +41,7 @@ def run(alt_val):
 
         span = np.NaN
 
-    return alt_val, span
+    return dist, span
 
 def plot_results(run_name):
     import matplotlib.pyplot as plt
@@ -51,7 +51,7 @@ def plot_results(run_name):
     # Do raw imports
     data = pd.read_csv(f"cache/{run_name}.csv")
     data.columns = data.columns.str.strip()
-    altitude = np.array(data['Altitude'], dtype=float)
+    altitude = np.array(data['Distance'], dtype=float)
     altitude = np.divide(altitude, 1000)
     spans = np.array(data['Spans'], dtype=float)
 
@@ -59,9 +59,9 @@ def plot_results(run_name):
 
     plt.plot(altitude, spans, ".-", label='Feasible Aircraft')
     plt.scatter(altitude[43], spans[43], marker = "o", color='r', label='Baseline Design')
-    plt.xlabel(r"Minimum Cruise Altitude [km]")
+    plt.xlabel(r"Required Headway Per Day [km]")
     plt.ylabel(r"Wing Span [m]")
-    plt.title(r"Effect of Minimum Cruise Altitude on Wingspan")
+    plt.title(r"Effect of Required Headway Per Day on Wingspan")
     plt.tight_layout()
     plt.legend()
     plt.savefig('/Users/annickdewald/Desktop/Thesis/Photos/' + run_name, dpi=300)
@@ -75,19 +75,19 @@ if __name__ == '__main__':
     l = 20
     with open(filename, "w+") as f:
         f.write(
-            f"{'Altitude'.ljust(l)},"
+            f"{'Distance'.ljust(l)},"
             f"{'Spans'.ljust(l)}\n"
         )
 
     ### Define sweep space
-    altitudes = np.linspace(12000, 20000, 50)
+    distances = np.linspace(0, 1e6, 50)
 
     ### Crunch the numbers
-    for alt_val in altitudes:
-            alt_val, span_val = run(alt_val)
+    for dist in distances:
+            dist, span_val = run(dist)
             with open(filename, "a") as f:
                 f.write(
-                    f"{str(alt_val).ljust(l)},"
+                    f"{str(dist).ljust(l)},"
                     f"{str(span_val).ljust(l)}\n"
                 )
 
