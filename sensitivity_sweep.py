90a1c6704f797062c2765aec110501c0f6a64ba3
ajdewald <62044249+ajdewald@users.noreply.github.com>
2022-12-02 10:32:28 -0500

    make a pareto front of optimal aircraft trading between revisit rate and span

--- sensitivity_sweep.py
+++ sensitivity_sweep.py
@@ -5,10 +5,10 @@ import aerosandbox.numpy as np
 import pandas as pd
 
 ### Set the run ID
-run_name = "wind_direction"
-parameter = "wind_direction"
-unit = "degrees"
-sweep_space = np.linspace(0, 400, 20)
+run_name = "pareto_front"
+parameter = "lam"
+unit = "[-]"
+sweep_space = np.linspace(0, 1, 11)
 
 def run(val):
     print("\n".join([
@@ -37,13 +37,15 @@ def run(val):
         opti.set_initial(opti.lam_g, sol.value(opti.lam_g))
 
         span = sol.value(wing_span)
+        revisit = sol.value(revisit_rate)
     except Exception as e:
         print("Fail!")
         print(e)
 
         span = np.NaN
+        revisit = np.NaN
 
-    return val, span
+    return val, span, revisit
 
 def plot_results(run_name):
     import matplotlib.pyplot as plt
@@ -53,20 +55,22 @@ def plot_results(run_name):
     # Do raw imports
     data = pd.read_csv(f"cache/{run_name}.csv")
     data.columns = data.columns.str.strip()
-    altitude = np.array(data[parameter], dtype=float)
+    values = np.array(data[parameter], dtype=float)
     # altitude = np.divide(altitude, 1000)
     spans = np.array(data['Spans'], dtype=float)
+    revisit = np.array(data['Revisit Rate'], dtype=float)
+
 
     sns.set(font_scale=1)
 
-    plt.plot(altitude, spans, ".-", label='Feasible Aircraft')
+    plt.plot(revisit, spans, ".-", label='Pareto Front')
     # plt.scatter(altitude[43], spans[43], marker = "o", color='r', label='Baseline Design')
-    plt.xlabel(f"{parameter} [{unit}]")
+    plt.xlabel(r"Revisit Rate [-]")
     plt.ylabel(r"Wing Span [m]")
-    plt.title(f"Effect of {parameter} on Wingspan")
+    plt.title(r"Set of Optimal Aircraft")
     plt.tight_layout()
     plt.legend()
-    plt.savefig('/Users/annickdewald/Desktop/Thesis/Photos/' + run_name, dpi=300)
+    plt.savefig('cache/' + run_name, dpi=300)
     plt.show()
 
 
@@ -78,6 +82,7 @@ if __name__ == '__main__':
     with open(filename, "w+") as f:
         f.write(
             f"{parameter.ljust(l)},"
+            f"{'Revisit Rate'.ljust(l)},"
             f"{'Spans'.ljust(l)}\n"
         )
 
@@ -86,10 +91,11 @@ if __name__ == '__main__':
 
     ### Crunch the numbers
     for val in values:
-            val, span_val = run(val)
+            val, span_val, revist_val = run(val)
             with open(filename, "a") as f:
                 f.write(
                     f"{str(val).ljust(l)},"
+                    f"{str(revist_val).ljust(l)},"
                     f"{str(span_val).ljust(l)}\n"
                 )
 
