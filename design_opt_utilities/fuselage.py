3102c220d2d8246cdf4bb50c3a2bab9aba9232b1
Peter Sharpe <pds@mit.edu>
2023-04-11 10:57:58 -0400

    make fuse_x_c and fuse_z_c, etc. be the same data type (list of arrays)

--- design_opt_utilities/fuselage.py
+++ design_opt_utilities/fuselage.py
@@ -2,6 +2,7 @@ import aerosandbox as asb
 import aerosandbox.numpy as np
 import aerosandbox.modeling.splines as splines
 
+
 def make_fuselage(
         boom_length,
         nose_length,
@@ -63,7 +64,7 @@ def make_fuselage(
         xsecs=[
             asb.FuselageXSec(
                 # TODO have Peter check this is the correct change
-                xyz_c = [fuse_x_c[i], 0, fuse_z_c[i]],
+                xyz_c=[fuse_x_c[i], 0, fuse_z_c[i]],
                 radius=fuse_radius[i]
             ) for i in range(len(fuse_x_c))
         ]
@@ -71,12 +72,13 @@ def make_fuselage(
 
     return fuse
 
+
 def make_payload_pod(
-    total_length,
-    nose_length,
-    tail_length,
-    fuse_diameter,
-    fuse_resolution = 10,
+        total_length,
+        nose_length,
+        tail_length,
+        fuse_diameter,
+        fuse_resolution=10,
 
 ) -> asb.Fuselage:
     ### Build the fuselage geometry
@@ -115,14 +117,14 @@ def make_payload_pod(
     ])
 
     fuse = asb.Fuselage(
-    name = "payload pod",
-    xsecs = [
-        asb.FuselageXSec(
-            # TODO have Peter check this is the correct change
-            xyz_c=[fuse_x_c[i], 0, fuse_z_c[i]],
-            radius=fuse_radius[i]
-        ) for i in range(len(fuse_x_c))
-    ]
+        name="payload pod",
+        xsecs=[
+            asb.FuselageXSec(
+                # TODO have Peter check this is the correct change
+                xyz_c=[fuse_x_c[i], 0, fuse_z_c[i]],
+                radius=fuse_radius[i]
+            ) for i in range(len(fuse_x_c))
+        ]
     )
     # print(fuse_x_c)
     # print(fuse_z_c)
@@ -130,14 +132,15 @@ def make_payload_pod(
 
     return fuse
 
-#Below is a function that takes in the diameter and length of the payload pod and returns a fuselage object with
+
+# Below is a function that takes in the diameter and length of the payload pod and returns a fuselage object with
 # an aerodynamic shape defined from the bezier spline function in aerosandbox
 def aero_payload_pod(
-    body_length, #length of the payload pod
-    diameter, #maximum diameter of the payload pod
-    nose_length, #length of the nose of the payload pod
-    tail_length, #length of the tail of the payload pod
-    resolution = 10,
+        body_length,  # length of the payload pod
+        diameter,  # maximum diameter of the payload pod
+        nose_length,  # length of the nose of the payload pod
+        tail_length,  # length of the tail of the payload pod
+        resolution=10,
 ) -> asb.Fuselage:
     fuse_x_c = []
     fuse_z_c = []
@@ -148,11 +151,11 @@ def aero_payload_pod(
         x_a=0,
         x_b=nose_length,
         y_a=0.00001,
-        y_b=diameter/2,
+        y_b=diameter / 2,
         dydx_a=4,
         dydx_b=0,
     )
-    z_n = [-diameter/2] * resolution
+    z_n = -diameter / 2 * np.ones(resolution)
     fuse_x_c.extend([x_n])
     fuse_z_c.extend([z_n])
     fuse_radius.extend([y_n])
@@ -160,33 +163,39 @@ def aero_payload_pod(
     x_t, y_t = splines.quadratic_bezier_patch_from_tangents(
         t=np.linspace(0, 1, resolution),
         x_a=body_length,
-        x_b=body_length+tail_length,
-        y_a=diameter/2,
+        x_b=body_length + tail_length,
+        y_a=diameter / 2,
         y_b=0.001,
         dydx_a=0,
-        dydx_b=-0.3,
+        dydx_b=-0.5,
     )
-    z_t = [-diameter/2] * resolution
+    z_t = -diameter / 2 * np.ones(resolution)
     fuse_x_c.extend([x_t])
     fuse_z_c.extend([z_t])
     fuse_radius.extend([y_t])
+
+    fuse_x_c = np.concatenate(fuse_x_c)
+    fuse_z_c = np.concatenate(fuse_z_c)
+    fuse_radius = np.concatenate(fuse_radius)
+
     fuse = asb.Fuselage(
-    name = "payload pod",
-    xsecs = [
-        asb.FuselageXSec(
-            xyz_c=[fuse_x_c[i], 0, fuse_z_c[i]],
-            radius=fuse_radius[i]
-        ) for i in range(len(fuse_x_c))
-    ]
+        name="payload pod",
+        xsecs=[
+            asb.FuselageXSec(
+                xyz_c=[fuse_x_c[i], 0, fuse_z_c[i]],
+                radius=fuse_radius[i]
+            ) for i in range(np.length(fuse_x_c))
+        ]
     )
 
     return fuse
 
+
 if __name__ == "__main__":
 
     opti = asb.Opti()
-    length= opti.variable(init_guess=2,lower_bound=1,upper_bound=3)
-    diameter = opti.variable(init_guess=0.5,lower_bound=0.3,upper_bound=0.7)
+    length = opti.variable(init_guess=2, lower_bound=1, upper_bound=3)
+    diameter = opti.variable(init_guess=0.5, lower_bound=0.3, upper_bound=0.7)
     payload_pod = aero_payload_pod(
         body_length=length,
         nose_length=0.5,
@@ -196,7 +205,3 @@ if __name__ == "__main__":
     volume = payload_pod.volume()
     opti.minimize(-volume)
     sol = opti.solve()
-
-
-
-
