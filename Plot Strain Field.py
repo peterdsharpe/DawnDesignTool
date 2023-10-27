935eabe1dfda55ba0f2912650650ec1fc27bd4a9
ajdewald <62044249+ajdewald@users.noreply.github.com>
2023-10-27 09:10:41 -0400

    add strain rate plot and improve plotting

--- Plot Strain Field.py
+++ Plot Strain Field.py
@@ -2,18 +2,22 @@ import numpy as np
 from aerosandbox.tools.pretty_plots import plt, sns, mpl, show_plot
 
 # define lower limit of spatial resolution where there are no benefits to further improving spatial resolution
-k_i = 100 # somewhere between 100 - 150 kPa/m^0.5
+k_i = 100 # somewhere between 100 - 150 kPa/m^0.5 as this is fracture toughness so this condition must be met
+## TODO try stepping through a range of k_i values and see how the plot changes with time as stress intensifies
 yield_strength = 100 # somewhere between 100 - 150 kPa
 poissons_ratio = 0.325 #
+A = 2.4 * 10 ** -24 # [Pa^-3s^-1] could be to the -25 if need be as this is melt point value and we don't expect that warm
 plastic_radius = k_i ** 2 / (2 * np.pi * yield_strength ** 2)
 
 spatial_resolution_lower_limit = 0.1 * plastic_radius
 
 print("spatial resolution lower limit: ", spatial_resolution_lower_limit)
 
+# calculate deviatoric stress and strain rate in ice around a crack
+n = 100
 # Define a range of r and θ values
-r_values = np.linspace(0.001, 15, 100)  # Adjust the range and number of points as needed
-theta_values = np.linspace(0, 2 * np.pi, 100)  # Adjust the range and number of points as needed
+r_values = np.linspace(0.001, 15, n)  # Adjust the range and number of points as needed
+theta_values = np.linspace(0, 2 * np.pi, n)  # Adjust the range and number of points as needed
 
 # Create a grid of r and θ values
 theta, radius = np.meshgrid(theta_values, r_values)
@@ -23,15 +27,16 @@ theta, radius = np.meshgrid(theta_values, r_values)
 # radius = 50
 
 # Initialize a 100x100 matrix to store the deviatoric stress values
-deviatoric_stress_matrix = np.zeros((100, 100))
+deviatoric_stress_matrix = np.zeros((n, n))
+strain_rate_matrix = np.zeros((n, n))
 
 # Initialize arrays to store Cartesian coordinates
-x_coords = np.zeros((100, 100))
-y_coords = np.zeros((100, 100))
+x_coords = np.zeros((n, n))
+y_coords = np.zeros((n, n))
 
 # Iterate through the grid and calculate deviatoric stress at each point
-for i in range(100):
-    for j in range(100):
+for i in range(n):
+    for j in range(n):
         r = radius[i, j]
         t = theta[i, j]
 
@@ -44,7 +49,7 @@ for i in range(100):
         y_coords[i, j] = y
 
         # Calculate the components of the Cauchy stress tensor
-        cauchy_stress_tensor = k_i / (2 * np.pi * r) ** 0.5 * np.array(
+        cauchy_stress_tensor = k_i / np.sqrt(2 * np.pi * r) * np.array(
             [
                 [np.cos(t / 2) * (1 - np.sin(t / 2) * np.sin(3 * t / 2)), np.sin(t / 2) * np.cos(t / 2) * np.cos(3 * t / 2)],
                 [np.sin(t / 2) * np.cos(t / 2) * np.cos(3 * t / 2), np.cos(t / 2) * (1 + np.sin(t / 2) * np.sin(3 * t / 2))]
@@ -56,10 +61,7 @@ for i in range(100):
         # Calculate the eigenvalues and mean stress
         eigenvalues = np.linalg.eigvals(cauchy_stress_tensor)
         I_1 = np.trace(cauchy_stress_tensor)
-        I_2 = eigenvalues[1]
-        I_3 = eigenvalues[2]
         pressure = (I_1) / 3
-        # pressure = (cauchy_stress_tensor[0, 0] + cauchy_stress_tensor[1, 1]) / 2
 
         deviatoric_stress_tensor = cauchy_stress_tensor - pressure * np.identity(3)
 
@@ -67,42 +69,69 @@ for i in range(100):
         deviatoric_stress = np.sqrt(
             (cauchy_stress_tensor[0, 0] - pressure) ** 2 + (cauchy_stress_tensor[1, 1] - pressure) ** 2
         )
-        deviatoric_stress = (np.trace(deviatoric_stress_tensor) ** 2 - np.trace((deviatoric_stress_tensor ** 2))) / 2
-        eigenvalues_tau = np.linalg.eigvals(deviatoric_stress_tensor)
-        I_1_tau = eigenvalues_tau[0]
-        # print(np.trace(deviatoric_stress_tensor))
-        I_2_tau = eigenvalues_tau[1]
-        # deviatoric_stress = I_2_tau
+        deviatoric_stress = np.sqrt((np.trace((deviatoric_stress_tensor ** 2))) / 2)
 
         # Store the deviatoric stress in the matrix
-        deviatoric_stress_matrix[i, j] = -deviatoric_stress
+        deviatoric_stress_matrix[i, j] = (deviatoric_stress)
+        strain_rate_matrix[i, j] =((A * deviatoric_stress ** 3) * 3.154e+7)
 
 # Create a contour plot of deviatoric stress in Cartesian coordinates
-viridis = mpl.colormaps.get_cmap('cividis')
+viridis = mpl.colormaps.get_cmap('viridis')
 newcolors = viridis(np.linspace(0, 1, 256))
-newcolors[-1, :] = np.array([76/255, 187/255, 23/255, 0.5])
+# newcolors[-1, :] = np.array([76/255, 187/255, 23/255, 0.5])
 newcolors[0, :] = np.array([0/255, 0/255, 0/255, 0.5])
 newcmp = mpl.colors.ListedColormap(newcolors)
+
 args = [
     x_coords,
     y_coords,
     deviatoric_stress_matrix,
 ]
+contour_levels = np.logspace(-1, 2, 10)
 kwargs = {
-    "levels": np.arange(0, 500, 25),
+    "levels": contour_levels,
     "alpha" : .9,
     "extend": "both",
 }
 plt.figure(figsize=(8, 8))
 CS = plt.contour(*args, **kwargs, colors="k", linewidths=0.2)
-contour = plt.contourf(*args, **kwargs, cmap=newcmp)
+contour = plt.contourf(*args, **kwargs, cmap=newcmp, locator=mpl.ticker.LogLocator())
 plt.plot([0, 1000], [0, 0], '-w', label='Crack Orientation')
 
 # Add a colorbar
 cbar = plt.colorbar(contour, label='Deviatoric Stress [kPa]')
 
 # Set the title and labels
-plt.title('Deviatoric Stress Contour Plot')
+plt.title('Deviatoric Stress Contour Plot Assuming Linear-Elastic Fracture Mechanics')
+plt.xlabel('X [meters]')
+plt.ylabel('Y [meters]')
+plt.xlim(-10, 10)
+plt.ylim(-10, 10)
+plt.legend(loc='upper left')
+
+# Show the plot
+plt.show()
+
+args = [
+    x_coords,
+    y_coords,
+    strain_rate_matrix,
+]
+kwargs = {
+    "levels": np.logspace(-20, -9, 12),
+    "alpha" : .9,
+    "extend": "both",
+}
+plt.figure(figsize=(8, 8))
+CS = plt.contour(*args, **kwargs, colors="k", linewidths=0.2)
+contour = plt.contourf(*args, **kwargs, cmap=newcmp, locator=mpl.ticker.LogLocator())
+plt.plot([0, 1000], [0, 0], '-w', label='Crack Orientation')
+
+# Add a colorbar
+cbar = plt.colorbar(contour, label='Strain Rate [1/year]')
+
+# Set the title and labels
+plt.title('Strain Rate Contour Plot')
 plt.xlabel('X [meters]')
 plt.ylabel('Y [meters]')
 plt.xlim(-10, 10)
