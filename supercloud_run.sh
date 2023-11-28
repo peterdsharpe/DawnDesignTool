d1398c212b6cc88a2619a74c9288a18010e21c15
ajdewald <62044249+ajdewald@users.noreply.github.com>
2023-11-27 19:30:09 -0500

    match batch run to electra version

--- supercloud_run.sh
+++ supercloud_run.sh
@@ -1,2 +1,10 @@
-module load anaconda/2021a
+#!/bin/bash
+
+# batch options
+#SBATCH --exclusive
+
+#loading the required module
+module load anaconda/2021b
+
+# run the script
 python span_vs_mission_run.py
