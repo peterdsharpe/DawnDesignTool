ee930ae633ca491e135cb8eb354c2d9afb20649e
ajdewald <62044249+ajdewald@users.noreply.github.com>
2024-02-12 17:05:34 -0500

    changing runs for some updated trajectory plots

--- ice_dyn_design_opt.py
+++ ice_dyn_design_opt.py
@@ -2262,7 +2262,7 @@ if __name__ == "__main__":
     import csv
     output_file = "sweeps_new_weighting3"
     runs =  [i for i in range(0, 168 + 1)]
-    runs = [0, 144, 146, 147, 148, 149, 150, 151, 152, 153, 154, 155, 156, 157, 158, 159, 160, 161, 162, 163, 164, 165, 166, 167, 168]
+    runs = [0, 1, 5, 6]
     combinations = []
     with open(f'outputs/{output_file}/0_parameter_combinations.csv', newline='\n') as csvfile:
         # Create a CSV reader object
