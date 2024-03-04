7aea21ce84bbad4af68cc652dd0f2ad975ab253e
ajdewald <62044249+ajdewald@users.noreply.github.com>
2024-03-04 11:28:44 -0500

    reformat to warm start each case and print outputs in their respective run_name folder

--- Parameter_sweep.py
+++ Parameter_sweep.py
@@ -47,7 +47,7 @@ def create_grid(file, resolution, coverage, day, precision, temporal):
 
     return combined_array
 
-def run(index_val, resolution_val, coverage_val, day_val, precision_val, temporal_val):
+def run(run_name, index_val, resolution_val, coverage_val, day_val, precision_val, temporal_val):
     print("\n".join([
         "-" * 50,
         f"Run number: {index_val}",
@@ -58,142 +58,148 @@ def run(index_val, resolution_val, coverage_val, day_val, precision_val, tempora
         f"Temporal: {temporal_val}"
     ]))
     run_num = index_val
-    opti.set_value(strain_azimuth_resolution, resolution_val)
-    opti.set_value(required_coverage_area, coverage_val)
-    opti.set_value(day_of_year, day_val)
-    opti.set_value(required_strain_precision, precision_val)
-    opti.set_value(required_strain_temporal_resolution, temporal_val)
-    if index_val == 0:
-        opti.set_value(track_scaler, 0)
-    else:
-        opti.set_value(track_scaler, 1)
+    opti.set_value(strain_azimuth_resolution, float(resolution_val))
+    opti.set_value(required_coverage_area, float(coverage_val))
+    opti.set_value(day_of_year, float(day_val))
+    opti.set_value(required_strain_precision, float(precision_val))
+    opti.set_value(required_strain_temporal_resolution, float(temporal_val))
+    opti.set_value(track_scaler, 0)
 
     try:
         sol = opti.solve(
-            max_iter=3000,
+            max_iter=30000,
             options={
-                "ipopt.max_cpu_time": 60000
+                "ipopt.max_cpu_time": 60000,
             },
             verbose=False
         )
 
-        print("Success!")
+        print("converged for track = 0 case!")
 
-        def s(x):  # Shorthand for evaluating the value of a quantity x at the optimum
-            return sol.value(x)
-
-        span = s(wing_span)
-
-        def output(x: Union[str, List[str]]) -> None:  # Output a scalar variable (give variable name as a string).
-            if isinstance(x, list):
-                for xi in x:
-                    output(xi)
-                return
-            # print(f"{x}: {sol.value(eval(x)):.3f}")
-
-
-        def fmt(x):
-            return f"{s(x):.6g}"
-
-        def plot(
-                x_name: str,
-                y_name: str,
-                xlabel: str,
-                ylabel: str,
-                title: str,
-                save_name: str = None,
-                show: bool = True,
-                plot_day_color=(103 / 255, 155 / 255, 240 / 255),
-                plot_night_color=(7 / 255, 36 / 255, 84 / 255),
-        ) -> None:  # Plot a variable x and variable y, highlighting where day and night occur
-
-            # Make the plot axes
-            fig, ax = plt.subplots(1, 1, figsize=(6.4, 4.8), dpi=plot_dpi)
-
-            # Evaluate the data, and plot it. Shade according to whether it's day/night.
-            x = s(eval(x_name))
-            y = s(eval(y_name))
-            plot_average_color = tuple([
-                (d + n) / 2
-                for d, n in zip(plot_day_color, plot_night_color)
-            ])
-            plt.plot(  # Plot a black line through all points
-                x,
-                y,
-                '-',
-                color=plot_average_color,
+        opti.set_initial_from_sol(sol)
+        opti.set_value(track_scaler, 1)
+        try:
+            sol = opti.solve(
+                max_iter=30000,
+                options={
+                    "ipopt.max_cpu_time": 60000,
+                },
+                verbose=False
             )
-            plt.plot(  # Emphasize daytime points
-                x[is_daytime],
-                y[is_daytime],
-                '.',
-                color=plot_day_color,
-                label="Day"
-            )
-            plt.plot(  # Emphasize nighttime points
-                x[is_nighttime],
-                y[is_nighttime],
-                '.',
-                color=plot_night_color,
-                label="Night"
-            )
+            print("converged for track = 1 case!")
 
-            # Disable offset notation, which makes things hard to read.
-            ax.ticklabel_format(useOffset=False)
+            def s(x):  # Shorthand for evaluating the value of a quantity x at the optimum
+                return sol.value(x)
 
-            # Do specific things for certain variable names.
-            if x_name == "hour":
-                ax.xaxis.set_major_locator(
-                    ticker.MultipleLocator(base=3)
+            span = s(wing_span)
+
+            def output(x: Union[str, List[str]]) -> None:  # Output a scalar variable (give variable name as a string).
+                if isinstance(x, list):
+                    for xi in x:
+                        output(xi)
+                    return
+                # print(f"{x}: {sol.value(eval(x)):.3f}")
+
+            def fmt(x):
+                return f"{s(x):.6g}"
+
+            def plot(
+                    x_name: str,
+                    y_name: str,
+                    xlabel: str,
+                    ylabel: str,
+                    title: str,
+                    save_name: str = None,
+                    show: bool = True,
+                    plot_day_color=(103 / 255, 155 / 255, 240 / 255),
+                    plot_night_color=(7 / 255, 36 / 255, 84 / 255),
+            ) -> None:  # Plot a variable x and variable y, highlighting where day and night occur
+
+                # Make the plot axes
+                fig, ax = plt.subplots(1, 1, figsize=(6.4, 4.8), dpi=plot_dpi)
+
+                # Evaluate the data, and plot it. Shade according to whether it's day/night.
+                x = s(eval(x_name))
+                y = s(eval(y_name))
+                plot_average_color = tuple([
+                    (d + n) / 2
+                    for d, n in zip(plot_day_color, plot_night_color)
+                ])
+                plt.plot(  # Plot a black line through all points
+                    x,
+                    y,
+                    '-',
+                    color=plot_average_color,
                 )
-            if y_name == "y_km":
-                # Create a rectangle patch
-                rectangle = mpl.patches.Rectangle((0, s(max_imaging_offset) / 1000), s(coverage_length) / 1000,
-                                                  s(coverage_width) / 1000, edgecolor='red',
-                                                  facecolor='red', linewidth=2, label='Coverage Area')
+                plt.plot(  # Emphasize daytime points
+                    x[is_daytime],
+                    y[is_daytime],
+                    '.',
+                    color=plot_day_color,
+                    label="Day"
+                )
+                plt.plot(  # Emphasize nighttime points
+                    x[is_nighttime],
+                    y[is_nighttime],
+                    '.',
+                    color=plot_night_color,
+                    label="Night"
+                )
 
-                # Add the rectangle patch to the axis
-                ax.add_patch(rectangle)
-            #     plt.xlim(-75, 150)
-            #     plt.ylim(-25, 125)
+                # Disable offset notation, which makes things hard to read.
+                ax.ticklabel_format(useOffset=False)
 
-            # Do the usual plot things.
-            plt.xlabel(xlabel)
-            plt.ylabel(ylabel)
-            plt.title(title)
-            plt.legend()
-            plt.tight_layout()
-            if save_name is not None:
-                plt.savefig(save_name)
+                # Do specific things for certain variable names.
+                if x_name == "hour":
+                    ax.xaxis.set_major_locator(
+                        ticker.MultipleLocator(base=3)
+                    )
+                if y_name == "y_km":
+                    # Create a rectangle patch
+                    rectangle = mpl.patches.Rectangle((0, s(max_imaging_offset) / 1000), s(coverage_length) / 1000,
+                                                      s(coverage_width) / 1000, edgecolor='red',
+                                                      facecolor='red', linewidth=2, label='Coverage Area')
 
+                    # Add the rectangle patch to the axis
+                    ax.add_patch(rectangle)
+                #     plt.xlim(-75, 150)
+                #     plt.ylim(-25, 125)
 
-            return fig, ax
+                # Do the usual plot things.
+                plt.xlabel(xlabel)
+                plt.ylabel(ylabel)
+                plt.title(title)
+                plt.legend()
+                plt.tight_layout()
+                if save_name is not None:
+                    plt.savefig(save_name)
 
+                return fig, ax
 
-        # Define the data for the "Outputs" section
-        outputs_data = {
-            "Wing Span": f"{fmt(wing_span)} meters",
-            "Strain Range Resolution": f"{fmt(strain_range_resolution)} meters",
-            "Strain Azimuth Resolution": f"{fmt(strain_azimuth_resolution)} meters",
-            "Strain Temporal Resolution": f"{fmt(strain_temporal_resolution)} hours",
-            "Strain Precision": f"{fmt(required_strain_precision)} 1 / yr",
-            "Coverage Area": f"{fmt(coverage_area)} meters^2",
-            "Day of Year": f"{fmt(day_of_year)}",
-            "Cruise Altitude": f"{fmt(cruise_altitude / 1000)} kilometers",
-            "Average Airspeed": f"{fmt(avg_airspeed)} m/s",
-            "Wing Root Chord": f"{fmt(wing_root_chord)} meters",
-            "mass_TOGW": f"{fmt(mass_total)} kg",
-            "Average Cruise L/D": fmt(avg_cruise_LD),
-            "CG location": "(" + ", ".join([fmt(xyz) for xyz in mass_props_TOGW.xyz_cg]) + ") m",
-        }
+            # Define the data for the "Outputs" section
+            outputs_data = {
+                "Wing Span": f"{fmt(wing_span)} meters",
+                "Strain Range Resolution": f"{fmt(strain_range_resolution)} meters",
+                "Strain Azimuth Resolution": f"{fmt(strain_azimuth_resolution)} meters",
+                "Strain Temporal Resolution": f"{fmt(strain_temporal_resolution)} hours",
+                "Strain Precision": f"{fmt(required_strain_precision)} 1 / yr",
+                "Coverage Area": f"{fmt(coverage_area)} meters^2",
+                "Day of Year": f"{fmt(day_of_year)}",
+                "Cruise Altitude": f"{fmt(cruise_altitude / 1000)} kilometers",
+                "Average Airspeed": f"{fmt(avg_airspeed)} m/s",
+                "Wing Root Chord": f"{fmt(wing_root_chord)} meters",
+                "mass_TOGW": f"{fmt(mass_total)} kg",
+                "Average Cruise L/D": fmt(avg_cruise_LD),
+                "CG location": "(" + ", ".join([fmt(xyz) for xyz in mass_props_TOGW.xyz_cg]) + ") m",
+            }
 
-        fmtpow = lambda x: fmt(x) + " W"
+            fmtpow = lambda x: fmt(x) + " W"
 
-        # Define a filename for the CSV file with the temporal resolution in the name
-        csv_file = f"outputs/outputs_data_run_{run_num}.csv"
+            # Define a filename for the CSV file with the temporal resolution in the name
+            csv_file = f"outputs/{run_name}/outputs_data_run_{run_num}.csv"
 
-        # Write the data to the CSV file
-        with open(csv_file, 'w', newline='') as csvfile:
+            # Write the data to the CSV file
+            with open(csv_file, 'w', newline='') as csvfile:
                 fieldnames = ["Property", "Value"]
                 writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
@@ -234,63 +240,62 @@ def run(index_val, resolution_val, coverage_val, day_val, precision_val, tempora
                 }.items():
                     writer.writerow({"Property": k, "Value": v})
 
-        print(f"Data from 'Outputs' section saved to {csv_file}")
+            print(f"Data from 'Outputs' section saved to {csv_file}")
 
-        ### Draw plots
-        plot_dpi = 200
+            ### Draw plots
+            plot_dpi = 200
 
-        # Find dusk and dawn
-        is_daytime = s(solar_flux_on_horizontal) >= 1  # 1 W/m^2 or greater insolation
-        is_nighttime = np.logical_not(is_daytime)
+            # Find dusk and dawn
+            is_daytime = s(solar_flux_on_horizontal) >= 1  # 1 W/m^2 or greater insolation
+            is_nighttime = np.logical_not(is_daytime)
 
-        plot("hour", "z_km",
+            plot("hour", "z_km",
                  xlabel="Hours after Solar Noon",
                  ylabel="Altitude [km]",
                  title="Altitude over Simulation",
-                 save_name=f"outputs/altitude_{run_num}.png"
+                 save_name=f"outputs/{run_name}/altitude_{run_num}.png"
                  )
-        plot("hour", "air_speed",
+            plot("hour", "air_speed",
                  xlabel="Hours after Solar Noon",
                  ylabel="True Airspeed [m/s]",
                  title="True Airspeed over Simulation",
-                 save_name=f"outputs/airspeed_{run_num}.png"
+                 save_name=f"outputs/{run_name}/airspeed_{run_num}.png"
                  )
-        plot("hour", "net_power",
+            plot("hour", "net_power",
                  xlabel="Hours after Solar Noon",
                  ylabel="Net Power [W] (positive is charging)",
                  title="Net Power to Battery over Simulation",
-                 save_name=f"outputs/net_power_{run_num}.png"
+                 save_name=f"outputs/{run_name}/net_power_{run_num}.png"
                  )
-        plot("hour", "battery_charge_state",
+            plot("hour", "battery_charge_state",
                  xlabel="Hours after Solar Noon",
                  ylabel="State of Charge [%]",
                  title="Battery Charge State over Simulation",
-                 save_name=f"outputs/battery_charge_{run_num}.png"
+                 save_name=f"outputs/{run_name}/battery_charge_{run_num}.png"
                  )
-        plot("hour", "distance",
+            plot("hour", "distance",
                  xlabel="hours after Solar Noon",
                  ylabel="Downrange Distance [km]",
                  title="Optimal Trajectory over Simulation",
-                 save_name=f"outputs/trajectory_{run_num}.png"
+                 save_name=f"outputs/{run_name}/trajectory_{run_num}.png"
                  )
-        plot("x_km", "y_km",
+            plot("x_km", "y_km",
                  xlabel="Downrange Distance in X [km]",
                  ylabel="Downrange Distance in Y [km]",
                  title="Optimal Trajectory over Simulation",
-                 save_name=f"outputs/trajectory_{run_num}.png"
+                 save_name=f"outputs/{run_name}/trajectory_{run_num}.png"
                  )
 
-        plt.savefig(f"outputs/mass_pie_chart_{run_num}.png")
-
-        if not parallel:
-            opti.set_initial_from_sol(sol)
+            plt.savefig(f"outputs/{run_name}/mass_pie_chart_{run_num}.png")
+        except Exception as e:
+            print('Failed to solve with track_scaler = 1')
+            print(e)
+            span = np.nan
 
     except Exception as e:
-        print("Fail!")
+        print("Does not converge with track_scaler = 0")
         print(e)
         span = np.nan
-
-
 
     return index_val, resolution_val, coverage_val, day_val, precision_val, temporal_val, span
 
